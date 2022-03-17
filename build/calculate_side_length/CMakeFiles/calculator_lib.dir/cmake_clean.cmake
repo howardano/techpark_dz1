f269b176604154e0656e8e40eab493ccaeef9fd8
howardano <58511197+howardano@users.noreply.github.com>
2022-03-17 17:19:38 +0000

    check valgrind

---
+++ build/calculate_side_length/CMakeFiles/calculator_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator_lib.dir/methods.c.o"
+  "libcalculator_lib.a"
+  "libcalculator_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calculator_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
